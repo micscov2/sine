6a60ae75a66039c0aed20fcb602a8715d46663fa
micscov2 <micscov2@gmail.com>
2017-05-18 16:08:53 +0530

    Added a method for checking closures

--- closure.py
+++ closure.py
@@ -1,11 +1,4 @@
-def counter():
-	x = 0
-	def abc():
-		x += 1
-		return x
-	return abc
-
-c = counter()
-print(c())
-print(c())
-print(c())
+def basegiver(base):
+	def nextstep(x):
+		return x + base
+	return nextstep
