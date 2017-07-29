6768527e336ea6119b217baed89e546658f0750e
micscov2 <micscov2@gmail.com>
2017-07-29 22:19:59 +0530

    Added some commands of cmd windows

---
+++ concepts/utils.bat
@@ -0,0 +1,11 @@
+rem Some windows commands
+rem 'rem' is used to comment out a line like # linux shell script
+rem 'assoc' command is used for displaying association of file types to file type/ program used to open them
+rem 'cipher' command is used write random data to 'something' so that it can't be recovered
+rem 'driverquery' is used to query drivers on a system
+rem 'fc' is used to compare two files
+rem 'netstat' same as linux command is used to similar functionality in windows
+rem 'pathping' is advanced version of ping
+rem 'tasklist' is similar to ps -eaf of linux
+rem 'taskkill' can be used to kill a task forcefully
+echo "hello world!"
